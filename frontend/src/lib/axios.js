3ec77ad56a22232835611b30c76227f5573b94d0
Ezekiel <jlewhite225@gmail.com>
2025-06-24 04:51:13 -0700

    update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:2205/api" : "/api"
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:2205/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
+
